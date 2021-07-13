31618bc1cdaf635012afd439491dd837b47275b3
MerilynMattos <merilyn.mattos@outlook.com>
2021-07-13 20:47:52 -0300

    responsividade parcial

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ for(const link of links){
   })
 }
 /* mudar o header da pagina quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', function(){
+
+
+function changeHeaderWhenScroll(){
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if(this.window.scrollY >= navHeight) {
     // scroll é maior que a altura  do header
     header.classList.add('scroll')
@@ -28,8 +30,7 @@ window.addEventListener('scroll', function(){
     // menor que a altura do header
     header.classList.remove ('scroll')
   }
-})
-
+}
 
 /* Testimonial carousel slider swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -49,4 +50,24 @@ const scrollReveal = ScrollReveal({
  reset: true 
 })
 
-scrollReveal.reveal(`#home .image, #home .text, #about .image, #about .text, #services header, #services .card,#testimonials header, #testimonials .testimonials, #contact .text, #contact .links`, { interval: 100 })
+scrollReveal.reveal(`#home .image, #home .text, #about .image, #about .text, #services header, #services .card,#testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`, { interval: 100 })
+
+/*Botão voltar para o topo*/
+
+function backToTop(){
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (this.window.scrollY >=560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+
+
+/* When Scroll*/
+window.addEventListener('scroll', function(){
+  changeHeaderWhenScroll() 
+  backToTop()
+})
