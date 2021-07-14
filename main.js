99ccc3d3857a0336afc24868f1dea4119085822b
MerilynMattos <merilyn.mattos@outlook.com>
2021-07-14 19:24:49 -0300

    corrigindo erro de declaração de function

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
@@ -12,60 +12,60 @@ for (const element of toggle) {
 /* quando clicar em um item do menu, esconder o menu*/
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links){
-  link. addEventListener('click', function(){
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 /* mudar o header da pagina quando der scroll*/
 
 const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const navHeight = header.offsetHeight
 
-function changeHeaderWhenScroll(){
-  
-  if(this.window.scrollY >= navHeight) {
+function changeHeaderWhenScroll() {
+  if (this.window.scrollY >= navHeight) {
     // scroll é maior que a altura  do header
     header.classList.add('scroll')
-  } else{
+  } else {
     // menor que a altura do header
-    header.classList.remove ('scroll')
+    header.classList.remove('scroll')
   }
 }
 
 /* Testimonial carousel slider swiper*/
 const swiper = new Swiper('.swiper-container', {
-   slidesPerView: 1,
-   pagination: {
-     el: '.swiper-pagination'
-   },
-   mousewheel: true,
-   keyboard:  true,
-   breakpoints: {
-     767: {
-       slidesPerView: 2,
-       setWrapperSize: true
-     }
-   }
-});
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
 
 /* SrollReveal: Mostrar elementos de scroll na pagina*/
 const scrollReveal = ScrollReveal({
- origin: 'top',
- distance: '30px',
- duration: 700,
- reset: true 
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
 })
 
-scrollReveal.reveal(`#home .image, #home .text, #about .image, #about .text, #services header, #services .card,#testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`, { interval: 100 })
+scrollReveal.reveal(
+  `#home .image, #home .text, #about .image, #about .text, #services header, #services .card,#testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`,
+  { interval: 100 }
+)
 
 /*Botão voltar para o topo*/
 const backToTopButton = document.querySelector('.back-to-top')
 
-function backToTop(){
-  
-
-  if (this.window.scrollY >=560) {
+function backToTop() {
+  if (this.window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
@@ -74,11 +74,10 @@ function backToTop(){
 
 /* Menu ativo conforme a seção visível na página*/
 const sections = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection(){
-
+function activateMenuAtCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerheight / 8) * 4
 
-  for( const section of sections ) {
+  for (const section of sections) {
     const sectionTop = section.offsetTop
     const sectionHeight = section.offsetHeight
     const sectionId = section.getAttribute('id')
@@ -86,27 +85,21 @@ function activateMenuAtCurrentSection(){
     const checkpointStart = checkpoint >= sectionTop
     const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
-    if(checkpointStart && checkpointEnd){
-      document.querySelector('nav ul li a[href*=' + sectionId + ']' )
-      .classList.add('active')
-
-
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
     } else {
-      document.querySelector('nav ul li a[href*=' + sectionId + ']' )
-      .classList.remove('active')
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
     }
-
   }
-
 }
 
 /* When Scroll*/
-window.addEventListener('scroll', function(){
-  changeHeaderWhenScroll() 
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
   backToTop()
-  function activateMenuAtCurrentSection()
+  activateMenuAtCurrentSection()
 })
-
-
-
-
